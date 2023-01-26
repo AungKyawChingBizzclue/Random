bb9f71567d60d3ed1643aeafc3e8bfe6a231272b
AungKyawChingBizzclue <122710285+AungKyawChingBizzclue@users.noreply.github.com>
2023-01-26 12:29:24 +0600

    Add files via upload

---
+++ 15 .js
@@ -0,0 +1,12 @@
+function mOver(x) {
+    x.innerHTML = "It's Not For Copy"
+}function mOut(y) {
+    y.innerHTML = "Ok! Try Again To Copy Me"
+}
+function mDown(a) {
+    a.style.backgroundColor = "yellow";
+    a.innerHTML = "Lol You Cant Copy";
+}function mUp(b) {
+    b.style.backgroundColor = "red";
+    b.innerHTML = "He He He! You Can't Copy Me"
+}
